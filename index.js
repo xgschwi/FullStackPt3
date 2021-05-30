12db306dc6b779bf250196bc2220614686d13862
xgschwi <xaviergschwind@gmail.com>
2021-05-30 00:04:04 -0400

    Exercise 3.7 Completed

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+//const logger = morgan('tiny')
+
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
