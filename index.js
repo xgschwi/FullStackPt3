d8ce0a2aef49c8d8f2964fd0850a3f847de44551
xgschwi <xaviergschwind@gmail.com>
2021-05-31 02:06:52 -0400

    Exercise 3.22 Completed

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const app = express()
 const Person = require('./models/person')
 const cors = require('cors')
 const morgan = require('morgan')
+
 // Logs content of request
-morgan.token('content', (req, res) => {
-    return JSON.stringify(req.body)
+morgan.token('content', (req) => {
+  return JSON.stringify(req.body)
 })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+const unknownEndpoint = (_request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(cors())
 app.use(express.static('build'))
@@ -21,94 +22,70 @@ app.use(express.json())
 
 
 
-const errHandler = (err, request, response, next) => {
-   
-    if(err.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    }
-    if(err.name === 'ValidationError') {  
-        return response.status(400).json({error: err.message})
-    }
-    next(err)
+const errHandler = (err, _request, response, next) => {
+  if(err.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if(err.name === 'ValidationError') {
+    return response.status(400).json({ error: err.message })
+  }
+  next(err)
 }
 
 
 // Displays all numbers from the people in the phonebook
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
-    .catch(err => next(err))
+app.get('/api/persons', (_request, response) => {
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
-        if(person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(err => next(err))  
-})
-
-// Displays information about phonebook
-app.get('/info', (request, response) => {
-    const size = persons.length
-
-    response.send(`<div>
-    <p>Phonebook has info for ${size} people </p>
-    <p>${new Date}</p>
-    </div>`
-    )
-
-})
-
-// Display information about a person in phonebook
-app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
+      if(person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(err => next(err))
 })
 
-// Deletes a person from the phonebook
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
+// Delete information about a person in phonebook
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then(_result => {
+      response.status(204).end()
+    })
+    .catch(err => next(err))
 })
-
-const generateId = () => {
-    return Math.round(Math.random()*100000)
-}
 
 // Creates a new person for the phonebook
 app.post('/api/persons', (request, response, next) => {
-    
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-    const p = Person(person)
+  const p = Person(person)
 
-    p.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  p.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(err => next(err))
-    
 })
 
 
 app.use(unknownEndpoint)
 app.use(errHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}`)
+  console.log(`Server running on Port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: {type: String, minLength: 3, required: true, unique: true},
-    number: {type: String, minLength: 8, required: true}
+  name: { type: String, minLength: 3, required: true, unique: true },
+  number: { type: String, minLength: 8, required: true }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject.__id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.__id
+    delete returnedObject.__v
+  }
 })
 
 personSchema.plugin(uniqueValidator)
