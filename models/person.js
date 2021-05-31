f03d88db2bc67186e83d0dac81a991eab002b1e8
xgschwi <xaviergschwind@gmail.com>
2021-05-30 22:44:37 -0400

    Updated Exercise 3.21

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
 
